e5984333180bde53cf776dc3418327b6c5ff4ace
enzo-mandine <55872776+enzo-mandine@users.noreply.github.com>
2021-10-26 16:04:29 +0200

    finalized gui

--- main.py
+++ main.py
@@ -1,43 +1,173 @@
+import sys
 import time
 from re import split
 import pygame
-from pygame.locals import *
 
 
-class Grid:
+class PyGame:
+    def __init__(self):
+        pygame.init()
+
+        self.run = True
+        self.H = 900
+        self.W = 900
+        self.clock = pygame.time.Clock()
+        self.data = None
+        self.file_content = None
+        self.font = pygame.font.SysFont('Roboto', 48)
+        self.screen = pygame.display.set_mode(size=(self.H, self.W), flags=0, depth=0, display=0, vsync=0)
+        self.solver = SudokuSolver()
+
+        self.colors = {
+            'black': (0, 0, 0),
+            'red': (255, 0, 0),
+            'white': (255, 255, 255),
+            'light': (200, 200, 200)
+        }
+
+        self.color_active = self.colors['white']
+        self.color_passive = self.colors['black']
+
+    def draw_solve(self):
+        while self.run:
+            for event in pygame.event.get():
+                self.close_event(event)
+                self.return_event(event)
+
+            self.screen.fill(self.colors['light'])
+            i, j = 0, 0
+            for y in range(9):
+                for x in range(9):
+                    res = str(self.data[y][x])
+                    if self.file_content[y][x] == res:
+                        self.screen.blit(self.font.render(res, True, self.colors['black']), (i + 40, j + 40))
+                    else:
+                        self.screen.blit(self.font.render(res, True, self.colors['red']), (i + 40, j + 40))
+                    i += 100
+                j += 100
+                i = 0
+
+            i = 0
+            while i <= 900:
+                if i == 0:
+                    pass
+                if i == 900:
+                    pygame.draw.rect(self.screen, self.colors['black'], (0, i - 5, self.H, 5))
+                    pygame.draw.rect(self.screen, self.colors['black'], (i - 5, 0, 5, self.W))
+                else:
+                    pygame.draw.rect(self.screen, self.colors['black'], (0, i, self.H, 5))
+                    pygame.draw.rect(self.screen, self.colors['black'], (i, 0, 5, self.W))
+                i += 100
+            pygame.display.update()
+
+        pygame.quit()
+
+    def draw_input(self):
+        user_text = ''
+
+        notice_text = 'Enter your file name:'
+        notice = self.font.render(notice_text, True, self.colors['black'])
+        notice_width, notice_height = self.font.size(notice_text)
+
+        input_rect = pygame.Rect(
+            notice_width + 20,
+            10,
+            2,
+            notice_height
+        )
+
+        active = False
+        while self.run:
+            for event in pygame.event.get():
+                self.close_event(event)
+
+                # if a mouse button is clicked
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    active = input_rect.collidepoint(event.pos)
+
+                # if a key is pressed down
+                if event.type == pygame.KEYDOWN:
+
+                    # block enter key
+                    if event.key == pygame.K_RETURN:
+                        if self.solver._set_file_content(user_text):
+                            self.solver._set_underlying_data()
+                            self.solver._solve()
+
+                            self.data = self.solver.data
+                            self.file_content = self.solver.file_content
+                            self.draw_solve()
+                            # pygame.display.update()
+                            return True
+                        else:
+                            self.draw_input()
+
+                    # handle backspace
+                    if event.key == pygame.K_BACKSPACE:
+                        user_text = user_text[:-1]
+                    else:
+                        user_text += event.unicode
+
+            # fill the background
+            self.screen.fill(self.colors['light'])
+
+            # define the color of the input if active or not
+            input_color = self.color_active if active else self.color_passive
+
+            # render the notice
+            self.screen.blit(notice, (10, 10))
+
+            # init text input surface
+            text_surface = self.font.render(user_text, True, self.colors['black'])
+
+            # render the input
+            self.screen.blit(text_surface, (input_rect.x + 5, input_rect.y))
+
+            # update part of the screen
+            pygame.display.flip()
+
+            # set the refresh rate
+            self.clock.tick(60)
+
+        pygame.quit()
+
+    def close_event(self, event) -> None:
+        try:
+            if event.type == pygame.QUIT:
+                self.run = False
+                sys.exit()
+        except AttributeError:
+            return False
+
+    def return_event(self, event) -> None:
+        try:
+            if event.key == pygame.K_ESCAPE:
+                self.draw_input()
+        except AttributeError:
+            return False
+
+        return True
+
+
+class SudokuSolver:
     def __init__(self):
         self.data = []
         self.file_content = None
 
-        self.BLACK = (0, 0, 0)
-        self.RED = (255, 0, 0)
-        self.BACKGROUND = (255, 255, 255)
-
-        # init
-        self._get_file_content()
-        self._set_underlying_data()
-        # self._print_grid()
-
-    def _get_file_content(self) -> None:
+    def _set_file_content(self, file_name: str) -> None:
         """ Open a text file and parse it to data """
-
-        while True:
-            # file_name = str(input("Enter your file name (only *.txt files allowed):\n"))
-            # file_name = str(input("Enter your file name:\n"))
-            file_name = "sample.txt"
-            try:
-                file = open('./' + file_name, 'r')
-                read = file.read()
-                file.close()
-                read = split(r"\n", read)
-                if file and len(read) == 9 or len(read[0]) == 9:
-                    break
-            except FileNotFoundError:
-                print("there is no such file, please try again")
+        file_name = file_name.lower()
+        try:
+            file = open('./' + file_name, 'r')
+            read = file.read()
+            file.close()
+            read = split(r"\n", read)
+        except FileNotFoundError:
+            return False
 
         self.file_content = read
 
-        return
+        return True
 
     def _set_underlying_data(self) -> None:
         """ From the file previously parsed, create the underlying data structure + blocks """
@@ -67,53 +197,6 @@ class Grid:
                 s += '-----------\n'
         print(s)
         return
-
-    def _draw(self, screen):
-        font = pygame.font.SysFont(None, 48)
-
-        i, j = 0, 0
-        for y in range(9):
-            for x in range(9):
-                if self.file_content[y][x] == str(self.data[y][x]):
-                    screen.blit(font.render(str(self.data[y][x]), True, self.RED), (i + 40, j + 40))
-                else:
-                    screen.blit(font.render(str(self.data[y][x]), True, self.BLACK), (i + 40, j + 40))
-                i += 100
-            i = 0
-            j += 100
-
-        i = 0
-        while i <= 900:
-            if i == 0:
-                pass
-            if i == 900:
-                pygame.draw.rect(screen, self.BLACK, (0, i - 5, 900, 5))
-                pygame.draw.rect(screen, self.BLACK, (i - 5, 0, 5, 900))
-            else:
-                pygame.draw.rect(screen, self.BLACK, (i, 0, 5, 900))
-                pygame.draw.rect(screen, self.BLACK, (0, i, 900, 5))
-            i += 100
-
-        return None
-
-    def gui(self):
-        pygame.init()
-        run = True
-        screen = pygame.display.set_mode(size=(900, 900), flags=0, depth=0, display=0, vsync=0)
-
-        # text part:
-
-        # sysfont = pygame.font.get_default_font()
-        while run:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-
-            screen.fill(self.BACKGROUND)
-            self._draw(screen)
-
-            pygame.display.update()
-        pygame.quit()
 
     def _get_empty_space(self) -> tuple or None:
         """ return a position of the first empty space found """
@@ -177,7 +260,7 @@ class Grid:
 
         return list({1, 2, 3, 4, 5, 6, 7, 8, 9} - set(in_board))
 
-    def solve(self):
+    def _solve(self):
         """ solve recursively the board """
 
         selected = self._get_empty_space()
@@ -202,7 +285,7 @@ class Grid:
                     self.data[y][x] = i
 
                     # call recursively previous step
-                    if self.solve():
+                    if self._solve():
                         return True
 
                     # int wasn't the good one
@@ -211,7 +294,14 @@ class Grid:
             return False
 
 
+def main():
+    # sudoku = SudokuSolver()
+    # sudoku.draw_solve()
+    # sudoku.draw_input()
+
+    pygame = PyGame()
+    pygame.draw_input()
+
+
 if __name__ == "__main__":
-    sudoku = Grid()
-    sudoku.solve()
-    sudoku.gui()
+    main()
